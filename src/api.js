fbe0be7ce338274fa2939c94f7e91e0dd3ac5b2c
alfred-richards <richards@dataterminal.co>
2025-02-28 18:07:52 +0530

    updating table names

--- src/api.js
+++ src/api.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 
-const endpoint = "http://localhost:4000/v1";
+const endpoint = process.env.REACT_APP_BACKEND_URL;
 
 export const getSalesOrders = async () => {
-  const data = await axios.get(endpoint + "/sales").then((res) => res.data);
+  const data = await axios.get(endpoint + "/fact_sales").then((res) => res.data);
   return data;
 };
 
 export const deleteOrderBook = async ({ column, value }) => {
   const data = await axios
-    .delete(endpoint + `/sales/${column}/${value}`)
+    .delete(endpoint + `/fact_sales/${column}/${value}`)
     .then((res) => res.data);
   return data;
 };
 
 export const getOrderDetails = async ({ orderId }) => {
   const data = await axios
-    .get(endpoint + `/sales/orderId/${orderId}`)
+    .get(endpoint + `/fact_sales/orderId/${orderId}`)
     .then((res) => res.data);
   return data;
 };
 
 export const addOrder = async ({ payload }) => {
-  return await axios.post(endpoint + "/sales", payload).then((res) => res.data);
+  return await axios.post(endpoint + "/fact_sales", payload).then((res) => res.data);
 };
